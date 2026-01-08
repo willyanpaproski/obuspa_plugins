9bb62680e94a50837b0766b93c14cf6c9596a9d1
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-08 17:57:28 -0300

    a

--- src/vendor/plugins/dhcpv4.c
+++ src/vendor/plugins/dhcpv4.c
@@ -7,15 +7,14 @@
 
 int GetGateway(dm_req_t *req, char *buf, int len)
 {
-    struct uci_context *ctx = NULL;
+    struct uci_context *ctx;
     struct uci_ptr ptr;
-    int ret = USP_ERR_OK;
-    char *uci_path = "dhcpv4.lan.start";
+    const char *uci_path = "dhcpv4.lan.router";
 
     (void)req;
 
-    if ((ctx = uci_alloc_context()) == NULL)
-    {
+    ctx = uci_alloc_context();
+    if (ctx == NULL) {
         return USP_ERR_INTERNAL_ERROR;
     }
 
@@ -23,17 +22,16 @@ int GetGateway(dm_req_t *req, char *buf, int len)
 
     if (uci_lookup_ptr(ctx, &ptr, uci_path, true) != UCI_OK ||
         ptr.o == NULL ||
-        ptr.o->type != UCI_TYPE_STRING ||
-        ptr.o->v.string == NULL ||
-        strlen(ptr.o->v.string) == 0)
+        ptr.o->type != UCI_TYPE_OPTION ||
+        ptr.o->v.string == NULL)
     {
-        snprintf(buf, len, "192.168.0.1");
-        goto exit;
+        snprintf(buf, len, "0.0.0.0");
+    }
+    else
+    {
+        snprintf(buf, len, "%s", ptr.o->v.string);
     }
 
-    snprintf(buf, len, "%s", ptr.o->v.string);
-
-exit:
     uci_free_context(ctx);
-    return ret;
+    return USP_ERR_OK;
 }
