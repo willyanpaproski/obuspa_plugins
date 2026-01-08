0a8884951ceb15fee59bcee27742f1caabcc4ea9
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-08 19:33:18 -0300

    feat: added uci helper

--- src/vendor/plugins/dhcpv4.c
+++ src/vendor/plugins/dhcpv4.c
@@ -1,40 +1,14 @@
 #include "dhcpv4.h"
 #include "usp_err_codes.h"
 
-#include <uci.h>
 #include <string.h>
 #include <stdio.h>
+#include <uciHelper.h>
 
 int GetGateway(dm_req_t *req, char *buf, int len)
 {
-    struct uci_context *ctx;
-    struct uci_ptr ptr;
-    char uci_path[64];
 
     (void)req;
 
-    snprintf(uci_path, sizeof(uci_path), "dhcpv4.lan.router");
-
-    ctx = uci_alloc_context();
-    if (ctx == NULL) {
-        return USP_ERR_INTERNAL_ERROR;
-    }
-
-    memset(&ptr, 0, sizeof(ptr));
-
-    if (uci_lookup_ptr(ctx, &ptr, uci_path, true) != UCI_OK ||
-        ptr.o == NULL ||
-        ptr.o->type != UCI_TYPE_STRING ||
-        ptr.o->v.string == NULL ||
-        ptr.o->v.string[0] == '\0')
-    {
-        snprintf(buf, len, "0.0.0.0");
-    }
-    else
-    {
-        snprintf(buf, len, "%s", ptr.o->v.string);
-    }
-
-    uci_free_context(ctx);
-    return USP_ERR_OK;
+    return GetStringValue("dhcpv4.lan.router", buf, len);
 }
---
+++ src/vendor/plugins/uciHelper.h
@@ -0,0 +1,6 @@
+#ifndef UCI_HELPER
+#define UCI_HELPER
+
+int GetStringValue(char *path, char *buf, int len);
+
+#endif
