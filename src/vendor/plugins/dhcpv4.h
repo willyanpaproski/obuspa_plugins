a0a42662ef540c5a2453c84d1d7fd82f4804d36f
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-08 20:18:08 -0300

    feat: added dhcpv4 getters

--- src/vendor/plugins/dhcpv4.h
+++ src/vendor/plugins/dhcpv4.h
@@ -8,6 +8,6 @@ int GetSubnetMask(dm_req_t *req, char *buf, int len);
 int GetMinAddress(dm_req_t *req, char *buf, int len);
 int GetMaxAddress(dm_req_t *req, char *buf, int len);
 int GetLeaseTime(dm_req_t *req, char *buf, int len);
-int GetEnable(dm_req_t *req, char *buf, int len)
+int GetEnable(dm_req_t *req, char *buf, int len);
 
 #endif
