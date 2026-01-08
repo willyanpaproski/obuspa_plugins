7dbc950b7e7b9b207673d0353ebd11da5ca0c6a7
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-08 19:38:41 -0300

    feat: added uci helper

--- src/vendor/plugins/dhcpv4.c
+++ src/vendor/plugins/dhcpv4.c
@@ -1,9 +1,9 @@
 #include "dhcpv4.h"
 #include "usp_err_codes.h"
+#include "uciHelper.h"
 
 #include <string.h>
 #include <stdio.h>
-#include <uciHelper.h>
 
 int GetGateway(dm_req_t *req, char *buf, int len)
 {
